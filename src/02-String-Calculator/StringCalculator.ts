59a827bbceb3549ab62866a562c94aec5bea300b
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2019-05-17 17:29:33 +0300

    Kata 2 - Core refactoring

--- src/02-String-Calculator/StringCalculator.ts
+++ src/02-String-Calculator/StringCalculator.ts
@@ -1,28 +1,37 @@
 class StringCalculator {
     add(str: string): number {
         let sum: number;
-        let delimiter: RegExp = /,|\n/;
-        let numbers: string[];
-        let negativeNums: string[];
 
         if (str === "") {
             sum = 0;
-        } else if (str.search(delimiter) !== -1) {
-            numbers = str.split(delimiter);
-            negativeNums = numbers.filter((val: string) => Number(val) < 0);
-
-            if (negativeNums.length > 0) {
-                throw new Error(`negatives not allowed: ${negativeNums.join(",")}`);
-            }
-
-            sum = numbers.reduce((acc: number, val: string) => {
-                return acc += Number(val);
-            }, 0);
         } else {
-            sum = Number(str);
+            sum = this.sum(
+                this.checkNegatives(
+                    this.splitAndParse(str)
+                )
+            );
         }
 
         return sum;
+    }
+
+    private splitAndParse(str: string): number[] {
+        const delimiter = /,|\n/;
+        return str.split(delimiter).map(Number);
+    }
+
+    private checkNegatives(numbers: number[]): number[] {
+        const negatives = numbers.filter((val: number) => val < 0);
+        if (negatives.length > 0) {
+            throw new Error(`negatives not allowed: ${negatives.join(",")}`);
+        }
+        return numbers;
+    }
+
+    private sum(numbers: number[]): number {
+        return numbers.reduce((acc: number, val: number) => {
+            return acc += val;
+        }, 0);
     }
 }
 
