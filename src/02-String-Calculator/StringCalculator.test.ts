a70f9ca96e7e68e4539352092f0dc0b443ab9570
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2019-05-17 16:44:18 +0300

    Kata 2 - Test 2

--- src/02-String-Calculator/StringCalculator.test.ts
+++ src/02-String-Calculator/StringCalculator.test.ts
@@ -4,4 +4,9 @@ describe("StringCalculator.add()", () => {
     it("should return 0 if we provide an empty string", () => {
         expect(StringCalculator.add("")).toBe(0);
     });
+
+    it("should return the single number", () => {
+        expect(StringCalculator.add("1")).toBe(1);
+        expect(StringCalculator.add("2")).toBe(2);
+    });
 });
